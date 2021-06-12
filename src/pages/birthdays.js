0b16fcfb17c997f7c3b9c7391a3e6be012bbb2de
Saisrinivenkat <saivenkatsrini@gmail.com>
2021-06-12 13:24:32 +0530

    first commit

---
+++ src/pages/birthdays.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function birthdays() {
+  return (
+    <div>
+      <h2>
+        NOT STARTED
+      </h2>
+    </div>
+  )
+}
---
+++ src/pages/home.js
@@ -0,0 +1,62 @@
+import React,{ useState } from 'react'
+import { Link } from "react-router-dom";
+import '../assets/main.css'
+import '../assets/custom.css'
+
+class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { toggle : 'hidden' }
+  }
+  render(){
+    return (
+      <div className="container">
+        <div className="grid-temp  h-screen bg-friends bg-center bg-cover bg-no-repeat">
+          <Navbar/>
+          <div className=" col-span-5">
+            <div className="flex flex-col items-center justify-center h-full">
+              <h1 className="font-rhyme text-4xl font-bold text-gray-50 tracking-widest">CBI</h1>
+              <h2 className="font-rhyme font-medium tracking-wider text-1xl text-white pt-6">THE NOTORIOUS GANG</h2>
+              <h2 className="font-rhyme font-medium text-1xl tracking-wider text-white pt-6 pb-10">MANDA PATHIRAM</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+function Navbar(){
+  const [view,changeview] = useState("hidden");
+  const [bgcolor,changecolor] = useState("none");
+
+  function shownav(){
+    if(view === 'hidden'){
+      changeview('flex');
+      changecolor("bg-gray-100")
+    }else{
+      changeview('hidden')
+      changecolor('none')
+    }
+    
+  }
+  return(
+    <header className={`col-span-5 ${bgcolor}`}>
+      <div className="flex md:hidden relative w-full h-10 cursor-pointer" onClick={shownav}>
+        <div className="absolute top-3 right-8">
+          <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" className="h-6 w-6" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+        </div>
+      </div>
+      <nav>
+        <ul className={`md:flex md:text-white md:flex-row justify-center flex-col  ${view}`}>
+          <li className="text-center text-2xl p-3 hover:bg-red-300 transition duration-300"><Link to="/" >Home</Link> </li>
+          <li className="text-center p-3 text-2xl hover:bg-red-300"><Link to="/bday" >Birthdays</Link> </li>
+        </ul>
+      </nav>
+    </header>
+  );
+
+}
+
+export default Home;
