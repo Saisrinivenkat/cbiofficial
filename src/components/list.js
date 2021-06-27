f41a6ad8ad612e8bdbff7bf819530afe0b86385a
Saisrinivenkat <saivenkatsrini@gmail.com>
2021-06-27 21:06:33 +0530

    fix-font size

--- src/components/list.js
+++ src/components/list.js
@@ -12,7 +12,7 @@ export default function List({data}) {
               <img src={image} className="h-10 w-10 md:h-14 md:w-14 rounded-full" alt={name} />
             </div>
             <div>
-              <h3 className="text-base md:text-xl font-medium text-indigo-400 cursor-pointer">{name}</h3>
+              <h3 className="text-xs md:text-xl font-medium text-indigo-400 cursor-pointer">{name}</h3>
               <p className="text-gray-500">{dob}</p>
             </div>
           </article>
